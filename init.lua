2cbfe1787f52fcdce11f7c83257b4c34acf2b6f4
mingo99 <2839131656@qq.com>
2024-09-19 23:20:16 +0800

    config color of name of user and group

--- init.lua
+++ init.lua
@@ -18,9 +18,9 @@ Status:children_add(function()
 	end
 
 	return ui.Line({
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-		ui.Span(":"),
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("#6495ED"),
+		ui.Span(":"):fg("#87CEFA"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("#6495ED"),
 		ui.Span(" "),
 	})
 end, 500, Status.RIGHT)
@@ -29,7 +29,7 @@ Header:children_add(function()
 	if ya.target_family() ~= "unix" then
 		return ui.Line({})
 	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("#87CEFA")
 end, 500, Header.LEFT)
 
 require("full-border"):setup({
